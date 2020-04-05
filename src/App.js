1caeacc606e2449f6011f4b1201bbe6eb9c0dc06
khalidm53 <khalid-muhammad@live.com>
2020-04-04 21:33:18 -0400

    updated my app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import React from "react";
-import Main from "./components/Main";
-import Wrapper from "./components/Wrapper";
-import Header from "./components/Header";
+import React, { Component } from "react";
+//import logo from './logo.svg';
+import UserList from "./components/UserList";
+import Title from "./components/Title";
+import Search from "./components/Search";
+import API from "./utils/API";
+import './App.css';
 
-import "./App.css";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <Wrapper>
-        <Header />
-        <Main />
-      </Wrapper>
-    </div>
-  );
+  state = {
+    result: [],
+    search: ""
+  };
+
+  componentDidMount() {
+    API.randomuser()
+      .then(res => this.setState({ result: res.data.results }))
+      .catch(err => console.log(err));
+  }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+
+    let newResult = this.state.result.filter(employee =>{
+      return employee.name.first.toLowerCase().indexOf(value)>-1 || employee.name.last.toLowerCase().indexOf(value)>-1
+    })
+    this.setState({
+      result: newResult
+    })
+    if (value.length===0){
+      API.randomuser()
+        .then(res => this.setState({ result: res.data.results }))
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+
+    return (
+      <div className="container">
+        <Title />
+        <div className="flex">
+          <div className="formbox">
+          <Search
+           value={this.state.search}
+           handleInputChange={this.handleInputChange}
+           />
+          </div>
+          <div className="listbox">
+            {this.state.result.map((element, index) => (
+              <UserList
+                key={index}
+                name={element.name.first + " " + element.name.last}
+                picture={element.picture.medium}
+                city={element.location.city}
+                email={element.email}
+                state={element.location.state}
+                />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
 
 export default {
-  // Gets all users
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
+    randomuser: function() {
+        return axios.get(BASEURL);
+    }
 };
